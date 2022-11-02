d21fe2678f90c37424f8f24b5ad1f51b5143b6fe
jihoonk-1116 <jihoonk1116@gmail.com>
2022-11-01 22:23:54 -0400

    commit2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          restaurant 2 
+          restaurant 3
         </a>
       </header>
     </div>
